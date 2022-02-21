cf58683bd1d8cdd3fe84d5ecfab3fb1e86933934
Zaki Pucci <78914874+zpucci@users.noreply.github.com>
2022-02-21 18:56:59 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,109 @@
+document.addEventListener("keydown", function (event) {
+    if (event.keyCode === 13) {
+        var text = "https://" + document.getElementById("input").value;
+        document.getElementById("iframe").src = text;
+        document.getElementById("iframe").height = "100%";
+        document.getElementById("iframe").width = "100%";
+        document.getElementById("content").innerHTML = '';
+    }
+});
+
+function youtube() {
+    document.getElementById("iframe").src = "https://teachersforteachers.herokuapp.com/search/hvtrs8%2F-wuw%2Cymuvu%60e%2Ccmm-/";
+    document.getElementById("iframe").height = "100%";
+    document.getElementById("iframe").width = "100%";
+    document.getElementById("content").innerHTML = '';
+}
+
+function spotify() {
+    document.getElementById("iframe").src = "https://thesimpleunblocker.com/main/hvtrs8%2F-wuw%2Csrovidy%2Ccmm-uq%2F/";
+    document.getElementById("iframe").height = "100%";
+    document.getElementById("iframe").width = "100%";
+    document.getElementById("content").innerHTML = '';
+}
+
+function spotify() {
+    document.getElementById("iframe").src = "http://thesimpleunblocker.com/main/hvtrs8%2F-wuw%2Csrovidy%2Ccmm-uq%2F/";
+    document.getElementById("iframe").height = "100%";
+    document.getElementById("iframe").width = "100%";
+    document.getElementById("content").innerHTML = '';
+}
+
+function Roblox() {
+    document.getElementById("iframe").src = "https://thesimpleunblocker.com/main/hvtrs8%2F-nmw%2Cge%2Frlcy-rmbnoz-aoppmrctkol%2F7369-rmbnoz/";
+    document.getElementById("iframe").height = "100%";
+    document.getElementById("iframe").width = "100%";
+    document.getElementById("content").innerHTML = '';
+}
+
+
+function rammerhead() {
+    document.getElementById("iframe").src = "https://sharks.cf";
+    document.getElementById("iframe").height = "100%";
+    document.getElementById("iframe").width = "100%";
+    document.getElementById("content").innerHTML = '';
+}
+
+function netflix() {
+    document.getElementById("iframe").src = "http://thesimpleunblocker.com/main/hvtrs8%2F-wuw%2Cngtdlkx%2Ccmm-/";
+    document.getElementById("iframe").height = "100%";
+    document.getElementById("iframe").width = "100%";
+    document.getElementById("content").innerHTML = '';
+}
+function fnf() {
+    document.getElementById("iframe").src = "https://fridaynightfunkin.io/";
+    document.getElementById("iframe").height = "100%";
+    document.getElementById("iframe").width = "100%";
+    document.getElementById("content").innerHTML = '';
+}
+
+function linuxvm() {
+    document.getElementById("iframe").src = "http://copy.sh/v86/";
+    document.getElementById("iframe").height = "100%";
+    document.getElementById("iframe").width = "100%";
+    document.getElementById("content").innerHTML = '';
+}
+
+function HolyUnlocker() {
+    document.getElementById("iframe").src = "https://teachersforteachers.herokuapp.com/";
+    document.getElementById("iframe").height = "100%";
+    document.getElementById("iframe").width = "100%";
+    document.getElementById("content").innerHTML = '';
+}
+function discord() {
+    document.getElementById("iframe").src = "https://teachersforteachers.herokuapp.com/";
+    document.getElementById("iframe").height = "100%";
+    document.getElementById("iframe").width = "100%";
+    document.getElementById("content").innerHTML = '';
+}
+var d = new Date();
+var n = d.getDay();
+var titles = ['Fourtane of the day: You chromebook will become fast and Bloons td 6 will auto download', 'Fourtane of the day: It will Snow outside', 'Fourtane of the day: School will get canceld', 'Fourtane of the day: It will hail outside and school will get canceled', 'Fourtane of the day: Amogus will be under your bed tonight', 'Fourtane of the day: You will max you clash of clans acount', 'Fourtane of the day: You will be the best Clash royale player'];
+
+document.getElementById("title").innerHTML = titles[n-1];
+
+var boxid = "title";
+window.onkeyup = function (event) {
+  if (event.keyCode == 13) {
+    document.getElementById(boxid).style.display = "none";
+  }
+ }
+
+var bolid = "Bottom";
+windows.onkeyup = function (event) {
+  if (event.keycode == 13) {
+    document.getElementById(bolid).style.display = "none";
+  }
+ }
+ function myFunction(id,a,b) {
+    var el = document.getElementById(id);
+    var hrefStr;
+    if(~el.href.indexOf(a)) {
+        hrefStr = el.href.replace(a, b);
+        el.href = hrefStr;
+    } else {
+        hrefStr = el.href.replace(b, a);
+        el.href = hrefStr;
+    }
+}
+
